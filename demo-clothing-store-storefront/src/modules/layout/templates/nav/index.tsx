de38f96822042814849fa45a820d709703394f9c
fardin072 <omurfaruquetalukder9@gmail.com>
2025-12-14 18:24:10 +0600

    logo fixed for mobile

--- demo-clothing-store-storefront/src/modules/layout/templates/nav/index.tsx
+++ demo-clothing-store-storefront/src/modules/layout/templates/nav/index.tsx
@@ -26,7 +26,7 @@ export default async function Nav() {
               <MobileMenu regions={regions} categories={categories} />
               <LocalizedClientLink
                 href="/"
-                className="flex ml-12 items-center hover:opacity-90 transition-opacity whitespace-nowrap"
+                className="flex small:ml-12 items-center hover:opacity-90 transition-opacity whitespace-nowrap"
                 data-testid="nav-store-link"
               >
                 <img
